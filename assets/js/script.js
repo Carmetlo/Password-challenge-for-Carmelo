c2d8bb1a3c4c1ac31afff3c67f41039211763186
Michael Carmelo <michaelcarmelo22@gmail.com>
2023-10-22 17:59:58 -0600

    Added some notes and checked on some things

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var characterLength = 8;
-
+//these are the included arrays to pull the password from
 var specialCharacterArray = ['!', 'S', '&', '^', '?', '+'];
 var lowerCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
     'i', 'j', 'k',
@@ -11,7 +11,7 @@ var upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
     'Z',];
 var numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8',
     '9',];
-
+//This creates the button and starts the javascript functions
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
@@ -20,13 +20,14 @@ function writePassword() {
 
     var passwordText = document.querySelector("#password")
     if (status) {
-        var newPassword = generatePassword(length,choiceArray);
+        var newPassword = generatePassword(length,choiceArray); 
         passwordText.value = newPassword;
     } else {
         passwordText.value = "";
 
     }
 }
+//this is the function for generating the random text and number parts from the array
 function generatePassword(characterLength,choiceArray) {
     var password = "";
     for (var i = 0; i < characterLength; i++) {
@@ -36,6 +37,9 @@ function generatePassword(characterLength,choiceArray) {
     }
     return password;
 }
+//this section displays all the prompts when choosing how your password is generated
+//I had the most difficult part with this section, I didnt know you could just provide so many IF characteristics
+//my tutor  helped guide me through this section and I understand so much more now
 function windowPrompts() {
     var choicestring = "";
     characterLength = parseInt(prompt(" How many characters would you like? (8 - 128 characters"));
